ffea065319270a547c781c464fed66d92639f833
Luna Kilborn <leland.kilborn@hotmail.com>
2022-09-05 21:50:07 -0400

    Add logging and verbosity argument
    I added the `env_logger` package, and setup a verbosity argument in clap
    so that the user can control the logging level more easily then with
    environment variables.

--- src/cli_parsing.rs
+++ src/cli_parsing.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use log::Level;
 
 #[derive(Parser)]
 #[clap(author = "Et-17 <leland.kilborn@hotmail.com")]
@@ -8,8 +9,20 @@ pub struct Cli {
     #[clap(required = true)]
     #[clap(parse(from_os_str))]
     pub files: Vec<std::path::PathBuf>,
+    #[clap(help = "Verbosity level")]
+    #[clap(short = 'v')]
+    #[clap(long = "verbosity")]
+    #[clap(default_value = "0")]
+    #[clap(value_parser = clap::value_parser!(u8).range(0..=4))]
+    #[clap(action = clap::ArgAction::Count)]
+    pub verbosity: u8,
 }
 
 pub fn parse_arguments() -> Cli {
-    Cli::parse()
+    let args: Cli = Cli::parse();
+    std::env::set_var(
+        "RUST_LOG",
+        Level::iter().nth(args.verbosity.into()).unwrap().as_str(),
+    );
+    return args;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+#[macro_use]
+extern crate log;
+
 mod cli_parsing;
 
 fn main() {
-    for ele in cli_parsing::parse_arguments().files {
-        println!("{:?}", ele);
+    let args = cli_parsing::parse_arguments();
+    env_logger::init();
+    info!("Starting up");
+    info!("Passed lambda files:");
+    for ele in args.files {
+        info!("  {:?}", ele);
     }
+    error!("Error message");
+    warn!("Warning message");
+    info!("Information message");
+    debug!("Debug message");
+    trace!("Trace message");
 }
